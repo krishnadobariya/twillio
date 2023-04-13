8f6d32c88242e2c340285df73d371ca02e10ed04
krishnadobariya <krishnadobariya3488@gmail.com>
2023-04-13 12:55:27 +0530

    ADD: TWILLIO U\I

---
+++ public/assets/webchat-appConfig.sample.js
@@ -0,0 +1,9 @@
+var appConfig = {
+    //  change the your AccountSid
+    accountSid: "ACc2818257209e013e901bbfb842e1e57b",
+    // change to your Flex Flow SID
+    flexFlowSid: "FO...",
+    colorTheme: {
+        overrides: brandedColors
+    }
+}
